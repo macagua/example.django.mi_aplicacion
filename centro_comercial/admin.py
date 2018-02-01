439b34ea2ef72f7b612cd71fa17a4de6b2c24c1e
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-01-31 21:47:07 -0400

    Agregar clases del modelo al admin de django

--- centro_comercial/admin.py
+++ centro_comercial/admin.py
@@ -1,3 +1,33 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
 
 # Register your models here.
+from .models import CentroComercial, Local, CategoriaLocal
+
+
+class CentroComercialAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ('id', 'name')
+    search_fields = ['name']
+    list_per_page = 10
+
+
+class LocalAdmin(admin.ModelAdmin):
+    list_display = ['id', 'local', 'name']
+    list_display_links = ('local', 'name')
+    search_fields = ['name']
+    list_filter = ('local', 'name')
+    list_per_page = 10
+
+
+class CategoriaLocalAdmin(admin.ModelAdmin):
+    list_display = ['id', 'local', 'name']
+    list_display_links = ('local', 'name')
+    search_fields = ['name']
+    list_filter = ('local', 'name')
+    list_per_page = 10
+
+admin.site.register(CentroComercial)
+admin.site.register(Local)
+admin.site.register(CategoriaLocal)
--- centro_comercial/models.py
+++ centro_comercial/models.py
@@ -7,15 +7,46 @@ from django.db import models
 class CentroComercial(models.Model):
     name = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name = "Centro Comercial"
+        verbose_name_plural = "Centro Comerciales"
+
+    def __unicode__(self):
+        return "%s" % (self.name)
+
+    def __str__(self):
+        return self.name
 
 class Local(models.Model):
     local = models.ForeignKey(CentroComercial, on_delete=models.CASCADE,
-                              related_name='locales_centro_comercial')
+                              related_name='locales_centro_comercial',
+                              verbose_name='Centro comercial')
     name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = "Local"
+        verbose_name_plural = "Locales"
+
+    def __unicode__(self):
+        return "%s (%s)" % (self.name, self.local)
+
+    def __str__(self):
+        return self.name
 
 
 class CategoriaLocal(models.Model):
     local = models.ForeignKey(Local, on_delete=models.CASCADE,
+                              blank=True, null=True,
                               related_name='categoria_local',
-                              blank=True, null=True)
+                              verbose_name='Local')
     name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = "Categoria de Local"
+        verbose_name_plural = "Categorias de Locales"
+
+    def __unicode__(self):
+        return "%s" % (self.name)
+
+    def __str__(self):
+        return self.name
